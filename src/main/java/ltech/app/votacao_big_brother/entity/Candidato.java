e63ef7ffd1ab42beef5fa5fe32838f67a80da96a
lucasolasz <olaszlucas072@gmail.com>
2025-07-30 01:11:17 -0300

    Ajusta a classe para montar o objeto no construtor

--- src/main/java/ltech/app/votacao_big_brother/entity/Candidato.java
+++ src/main/java/ltech/app/votacao_big_brother/entity/Candidato.java
@@ -7,12 +7,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Candidato {
 
     @Id
