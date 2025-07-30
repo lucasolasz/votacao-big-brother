fb9bbcf48712141ff3b71bb1336a3bc39db308b4
lucasolasz <olaszlucas072@gmail.com>
2025-07-30 01:10:31 -0300

    Implementa o metodo salvar

--- src/main/java/ltech/app/votacao_big_brother/service/VotoService.java
+++ src/main/java/ltech/app/votacao_big_brother/service/VotoService.java
@@ -1,19 +1,32 @@
 package ltech.app.votacao_big_brother.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import ltech.app.votacao_big_brother.dto.CandidatoRecord;
+import ltech.app.votacao_big_brother.entity.Voto;
+import ltech.app.votacao_big_brother.repository.VotoRepository;
 
 @Service
 @AllArgsConstructor
 public class VotoService {
 
+    private final VotoRepository votoRepository;
     private final RabbitTemplate rabbitTemplate;
+    private final CandidatoService candidatoService;
 
     public void processarVoto(CandidatoRecord candidatoRecord) {
         rabbitTemplate.convertAndSend("computar-voto.ex", "", candidatoRecord);
+    }
+
+    public void save(CandidatoRecord candidatoRecord) {
+        var voto = new Voto();
+        voto.setHoraVoto(LocalDateTime.now());
+        voto.setCandidatoVotado(candidatoService.findById(candidatoRecord.id()));
+        votoRepository.save(voto);
     }
 
 }
